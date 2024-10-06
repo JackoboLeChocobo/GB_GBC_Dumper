5ff4fd2c89a4cc90e2d362151db705748f80c9ac
Jackobo Le Chocobo <imbert.jacquesolivier@gmail.com>
2024-10-06 09:47:20 +0200

    Fix Card Detection
    No need to do the weird Arduino IDE manipulation

---
+++ software/GB_GBC_Dumper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make
+chmod 777 $1
+stty 115200 -F $1 raw -echo
+./run $1 $2 $3
+
+echo " "
+echo "You can power off the Arduino card and change your cartridge before doing a new try"
+echo " "
